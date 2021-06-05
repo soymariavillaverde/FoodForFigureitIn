8f9f9706b382ecfffa2de28a5161b42e457ebf6a
Exodya21 <jaumegomiciai@gmail.com>
2021-06-05 13:28:35 +0200

    AcomodandoEldisplay

--- js-content/displayCarro.js
+++ js-content/displayCarro.js
@@ -1,8 +1,24 @@
-function mostrarCarrito() {
-    let x = document.querySelector(".carrito-compra");
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
+function mostrarCarrito() { /* Onclick en el boton naranja */
+    let carro = document.querySelector(".carrito-compra"); /* selecciono todo el carro */
+    let paginaPrincipal = document.getElementById("paginaPrincipal"); /* selecciono toda la pagina principal*/
+    
+    if (carro.style.display === 'none') { /* si el carro no se ve ... */
+        carro.style.display = 'block'; /* mostrará el carro */
+        paginaPrincipal.style.display = 'none'; /* ocultará la pagina principal*/
+        
+    } else {                           /* Si no ... */
+        carro.style.display = 'none';  /* no lo muestra */
+    }
+}
+
+function mostrarPrincipal() { /* onclick en el boton back del carrito, es lo mismo pero cambiando block por none */
+    let carro = document.querySelector(".carrito-compra");
+    let paginaPrincipal = document.getElementById("paginaPrincipal");
+
+    if (carro.style.display === 'block') {
+        carro.style.display = 'none';
+        paginaPrincipal.style.display = 'block';
     } else {
-        x.style.display = 'none';
+        carro.style.display = 'block';
     }
 }
