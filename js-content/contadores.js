9e496594a5b563ddeafc0f5131b579bc54124577
Exodya21 <jaumegomiciai@gmail.com>
2021-06-05 20:42:40 +0200

    ultimoPush

--- js-content/contadores.js
+++ js-content/contadores.js
@@ -2,11 +2,18 @@ function añadirPlato(botonSuma) {
     let padreBotonSuma = botonSuma.parentNode;
     let contador = padreBotonSuma.querySelector(".numeroContador");
     
+    let localizacion = document.getElementById('topCarrito');
+    let visaAbueloSuma = padreBotonSuma.parentNode.parentNode;
+
     if (contador.value < 9){
         contador.value++
     }
 
-    if (contador.value == 11){
+    if (contador.value >0){
+        localizacion.insertAdjacentElement("afterend",visaAbueloSuma);
+    }
+
+    if (contador.value == 11){      /* ejemplo en donde funciona el parseInt() */
         contador.value = parseInt(contador.value) * 10;
     }
 }
@@ -14,6 +21,8 @@ function añadirPlato(botonSuma) {
 function quitarPlato(botonResta) {
     let padreBotonResta = botonResta.parentNode;
     let contador = padreBotonResta.querySelector(".numeroContador");
+
+    
 
     if (contador.value > 0) {
         contador.value--
---
+++ js-content/displayPlatosCarro.js
@@ -0,0 +1,35 @@
+/* Hacer un queryselectorAll de los platos para tener una lista
+    hacer una lista de los platos del carrito
+    una funcion que con Onclick detecte si suma añada ese plato al carrito
+    una funcion que con Onclick detecte si resta quite ese plato del carrito
+    otra funcion que detecte que el plato que esta en el carrito tenga un valor >0 y haga block
+    otra funcion que detecte que el plato que esta en el carrito tenga un valor ==0 y haga none
+
+    funcion de vaciar carrito
+
+*/ 
+
+let listaPlatosNode = document.querySelectorAll(".plato-individual");
+let listaPlatos = Array.from(listaPlatosNode);
+
+let listaCarrito = [];
+
+let akfafn = document.querySelectorAll(".mas");
+let listaSumadores = Array.from(akfafn);
+
+
+
+/*
+console.log(listaPlatos[0]);
+let cosa = listaPlatos[0].querySelector(".numeroContador");
+console.log(cosa);
+let cosaVariable = cosa.value;
+console.log(cosaVariable);
+*/
+
+function detectarSuma(btnSuma) {
+    let padreBtnSuma = btnSuma.parentNode;
+    let contador = padreBtnSuma.querySelector(".numeroContador");
+
+    console.log(padreBtnSuma);
+}
