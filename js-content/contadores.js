6dc70690120156394e3295f6387962e87f9c6c8f
Exodya21 <jaumegomiciai@gmail.com>
2021-06-10 20:56:47 +0200

    retoquesAntesdeHacerLaFactura

--- js-content/contadores.js
+++ js-content/contadores.js
@@ -1,108 +1,50 @@
-
-/*let muchosPlatos = [
-    {
-        "id": 0,
-        "img": "Img/Imagenes comida/imagenes restaurant-01.jpg",
-        "nombre": "Chicken Veggi Salad",
-        "precio": 4.99,
-        "descripcion":"Tomate, lechuga, calabacín, cebolla y pollo a la plancha.",
-        "cantidad": 0
-    },
-    {
-        "id": 1,
-        "img": "Img/Imagenes comida/imagenes restaurant-02.jpg",
-        "nombre": "Mutton Curry",
-        "precio": 7.99,
-        "descripcion":"Curry con cebolleta y carne de vaca.",
-        "cantidad": 0
-    },
-    {
-        "id": 2,
-        "img": "Img/Imagenes comida/imagenes restaurant-03.jpg",
-        "nombre": "French Fries",
-        "precio": 2.99,
-        "descripcion":"Patatas muy crujientes y alargadas con salsa brava.",
-        "cantidad": 0
-    },
-    {
-        "id": 3,
-        "img": "Img/Imagenes comida/batido-mango-m.jpeg",
-        "nombre": "Batido de Mango",
-        "precio": 1.99,
-        "descripcion":"Mango, leche y hielo picado.",
-        "cantidad": 0
-    },
-    {
-        "id": 4,
-        "img": "Img/Imagenes comida/imagenes restaurant-04.jpeg",
-        "nombre": "Desayuno cargado",
-        "precio": 3.99,
-        "descripcion":"Tomate, chorizo, lomo, campiñones, habas, huevo frito y pan.",
-        "cantidad": 0
-    },
-    {
-        "id": 5,
-        "img": "Img/Imagenes comida/imagenes restaurant-05.jpeg",
-        "nombre": "Bocadillo de lomo y queso",
-        "precio": 1.99,
-        "descripcion":"Lomo, queso, cilantro.",
-        "cantidad": 0
-    },
-    {
-        "id": 6,
-        "img": "Img/Imagenes comida/imagenes restaurant-06.jpg",
-        "nombre": "Cake du chocolat",
-        "precio": 3.99,
-        "descripcion":"Chocolate, chocolate negro y nata.",
-        "cantidad": 0
-    },
-    {
-        "id": 7,
-        "img": "Img/Imagenes comida/imagenes restaurant-07.jpg",
-        "nombre": "Hamburguesa con queso",
-        "precio": 3.99,
-        "descripcion":"Carne de vaca, lechuga, quese cheddar y pan con sésamo.",
-        "cantidad": 0
-    }];
-
-let contadorArray = cartaDePlatos.map(function(obj){
-    return obj.cantidad 
-});
-//Funcio Suma 
-
-let displayCarrito = [];
-//imprimir(displayCarrito, ".platosCarrito")
-
-
+// Funciones suma resta y display block y none platos carrito  -----------------------------------
+let carrito = []; //imprimir(carrito, ".platosCarrito"); boton naranjaaa
+//Funcio Suma añadir a carrito                                     
 function añadirPlato(botonSuma, iD) {
-    let padreBotonSuma = botonSuma.parentNode;
-    let contador = padreBotonSuma.querySelector(".numeroContador");
-    
-    cartaDePlatos[iD].cantidad = cartaDePlatos[iD].cantidad + 1;
-
-    contador.value = cartaDePlatos[iD].cantidad;
-
+    upDateContador(botonSuma, siExisteEnCarritoSum( encontrarObj(iD) ) )
 }
+function encontrarObj(iD){
+    let objFound = cartaDePlatos.find(plato => plato.id == iD);
+    return objFound
+}
+function siExisteEnCarritoSum(objExiste){ // si no esta en el carrito suma y añade
+    if (carrito.includes(objExiste) == false){
+        objExiste.cantidad++;
+        carrito.push(objExiste)
+    }else {                              // si, si esta en el carrito solo suma
+        objExiste.cantidad++;
+    }
+    return objExiste.cantidad
+}
+function upDateContador(elementHTML, objValueCantidad){
+    let padreElementHTML = elementHTML.parentNode;
+    let contador = padreElementHTML.querySelector(".numeroContador");
 
-//Funcion Diplay Carrito
-
-
-displayCarrito = cartaDePlatos.filter(plato => plato.cantidad > 0)
-console.log(displayCarrito)
-
-imprimir(displayCarrito, ".platosCarrito")
-
+    contador.value = objValueCantidad;
+}
+function siExisteEnCarritoRest(objExiste){ 
+    if (carrito.includes(objExiste) == true){ // si es verdad que esta en el carrito y es >0 restara
+        if (objExiste.cantidad > 0) {
+            objExiste.cantidad--;
+        }
+        if (objExiste.cantidad == 0) {       // si la cantidad es 0 se eliminara del carrito
+            let index = carrito.indexOf(objExiste)
+            //console.log(index)
+            //console.log(carrito)
+            carrito.splice(index, 1);
+            //console.log(carrito)
+            imprimir(carrito, ".platosCarrito")
+        }
+    }
+    return objExiste.cantidad
+}
 //Funcion Resta
-
-function quitarPlato(botonResta, id) {
-    let padreBotonResta = botonResta.parentNode;
-    let contador = padreBotonResta.querySelector(".numeroContador");
-    
-    if (contadorArray[id] > 0){
-        contadorArray[id] = contadorArray[id] - 1;
-
-        contador.value = contadorArray[id];
-    }     
-
+function quitarPlato(botonResta, iD) {
+    upDateContador(botonResta, siExisteEnCarritoRest( encontrarObj(iD) ) )
 }
-*/
+//Boton vaciar carrito --------------------------------------------------------------------------
+function vaciarCarrito(){
+    carrito = [];
+    imprimir(carrito,".platosCarrito");
+}
